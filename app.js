cb992988ed26038138679c746cb976dc22282db7
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-06-24 20:14:33 +0700

    Add chat mechanism

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var app     = express();
+var io      = require("socket.io")();
 
 app.engine("html", require("ejs").renderFile);
 app.get("/", home);
-app.listen(4000);
+io.listen(app.listen(4000));
 
 function home(req, res) {
 	res.render("index.html");
 }
+
+io.on("connection", socket => {
+	socket.send("Welcome to our chat system");
+});
